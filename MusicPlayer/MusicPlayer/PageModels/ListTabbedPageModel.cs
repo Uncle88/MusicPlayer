f787a4d36ba4d1135fe6efad1293bca8e4163d55
Andrey <andreyproskura88@gmail.com>
2020-11-14 23:11:45 +0200

    - added sorting track list for separate tabs

--- MusicPlayer/MusicPlayer/PageModels/ListTabbedPageModel.cs
+++ MusicPlayer/MusicPlayer/PageModels/ListTabbedPageModel.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
 using MusicPlayer.Model;
 using MusicPlayer.Services.PlayService;
 using Xamarin.Forms;
@@ -12,17 +15,31 @@ namespace MusicPlayer.PageModels
 
         public List<TrackModel> Items { get; set; }
 
+        public ICommand SelectedArtistTabCommand => new Command(SelectedArtistTabCommandExecute);
+        public ICommand SelectedAlbumTabCommand => new Command(SelectedAlbumTabCommandExecute);
+        public ICommand SelectedGenreTabCommand => new Command(SelectedGenreTabCommandExecute);
+
         public ListTabbedPageModel()
         {
             audioService = DependencyService.Get<IPlayAudio>();
-            Items = tracksList = audioService.GetTrackModelList();
+            tracksList = audioService.GetTrackModelList();
+
+            Items = tracksList.OrderBy(x => x.Artist).ToList();
         }
 
-        public override void Initialize()
+        private void SelectedArtistTabCommandExecute()
         {
-            base.Initialize();
+            Items = tracksList.OrderBy(x => x.Artist).ToList();
+        }
 
-            
+        private void SelectedAlbumTabCommandExecute()
+        {
+            Items = tracksList.OrderBy(x => x.Album).ToList();
+        }
+
+        private void SelectedGenreTabCommandExecute()
+        {
+            Items = tracksList.OrderBy(x => x.Genre).ToList();
         }
     }
 }
